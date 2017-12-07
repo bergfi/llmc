eec13cbb0c33556d98e95fccbc75bde7418d610c
Freark van der Berg <fivanderberg@gmail.com>
2017-12-07 14:25:11 +0100

    Updates test call_arguments

--- tests/correctness/call_arguments/test.c
+++ tests/correctness/call_arguments/test.c
@@ -2,18 +2,18 @@
 
 int g = 5;
 
-void lol(int v) {
-	int i = 0,j = 0;
+void lol(int v, int i) {
+	int j = 0;
 	i++;
 	j++;
-	g = v;
+	g = v + i;
 }
 
 int main(int argc, char** argv) {
 	int i = 5;
-	lol(10);
+	lol(10, i);
 	i += g;
-	assert(i==15);
+	assert(i==21);
 	return i;
 }
 
